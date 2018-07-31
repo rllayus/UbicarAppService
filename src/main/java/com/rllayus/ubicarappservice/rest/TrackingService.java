eaee289795589072eaa9ca76161b7b2b3e9ddaa1
Cesar Augusto <cesar.aug.eng@gmail.com>
2018-07-31 10:30:36 -0400

    cambios de division cero

--- src/main/java/com/rllayus/ubicarappservice/rest/TrackingService.java
+++ src/main/java/com/rllayus/ubicarappservice/rest/TrackingService.java
@@ -29,7 +29,8 @@ public class TrackingService {
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("/saveLocations")
     public ObjectResponse<Integer> saveLocations(@Context HttpServletRequest httpRequest, ListRequest<LocationDto> request){
-        int a = 1/0;
+        int a =0 ;
+        int b = 1/a;
         LOGGER.info("Lista de localizaciones recibidas con :"+request.getValues().size());
         return this.coreAppHomeService.saveLocations(httpRequest,request);
     }
