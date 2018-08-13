0369287df0d75eaafb475cd6232ca63fa53c7ce3
Cesar Augusto <cesar.aug.eng@gmail.com>
2018-08-12 21:50:57 -0400

    Test

--- src/main/java/com/rllayus/ubicarappservice/bussiness/Calculadora.java
+++ src/main/java/com/rllayus/ubicarappservice/bussiness/Calculadora.java
@@ -7,7 +7,7 @@ public class Calculadora {
     public int suma(int a, int b){
         return a + b;
     }
-    public long multiplicacion(int a , int b ){
+    public int multiplicacion(int a , int b ){
         return a * b;
     }
 }
